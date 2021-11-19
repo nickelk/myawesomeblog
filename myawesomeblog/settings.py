59f4dfed61583d0b713c3ea34f111fde685e2c8b
nickelk <nick_smith@ukr.net>
2021-11-19 21:21:47 +0200

    postgresql set as db

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'myawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',  # 127.0.0.1
+        'PORT': '5432',
     }
 }
 
