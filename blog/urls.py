cc65730416ec52683e1e13da9f4d0f503b73bc9a
nickelk <nick_smith@ukr.net>
2021-11-22 16:25:21 +0200

    specific post template added

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog import views
+from . import views
 
 urlpatterns = [
-    path('', views.showblog, name='showblog')
+    path('', views.showblog, name='showblog'),
+    path('<int:post_id>', views.specific_post, name='specific_post'),
 ]
