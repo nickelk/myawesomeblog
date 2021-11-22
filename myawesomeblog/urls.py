28c0cf73249c33a527207a7899d3141aaa5dbaad
nickelk <nick_smith@ukr.net>
2021-11-22 12:11:48 +0200

    home template added

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+import events.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
