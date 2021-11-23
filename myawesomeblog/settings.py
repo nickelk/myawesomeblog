eab7f289a966d26fd728943f500b814002f9a4ba
nickelk <nick_smith@ukr.net>
2021-11-23 09:00:37 +0200

    static files added

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'myawesomeblog/static',
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
