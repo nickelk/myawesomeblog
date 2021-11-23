f9ed8de103dd0caf6e267c90e75fd96efc8d9392
nickelk <nick_smith@ukr.net>
2021-11-23 10:50:17 +0200

    templates and references tweaked

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,4 @@ def showblog(request):
 
 def specific_post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'blog/specific_post.html', {'specific_post': post})
+    return render(request, 'blog/specific_post.html', {'post': post})
